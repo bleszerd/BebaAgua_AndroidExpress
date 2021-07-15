a8aeb0a6ce4b46b12c94c791d80acba89b02f05a
Vinicius Resende <alive2k@programmer.net>
2021-07-15 18:06:55 -0300

    Add notifications

--- app/src/main/java/bleszerd/com/github/beberagua/MainActivity.java
+++ app/src/main/java/bleszerd/com/github/beberagua/MainActivity.java
@@ -1,18 +1,24 @@
 package bleszerd.com.github.beberagua;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnNotify;
@@ -39,9 +45,7 @@ public class MainActivity extends AppCompatActivity {
         btnNotify.setOnClickListener(notifyClick);
 
         timePicker.setIs24HourView(true);
-
         preferences = getSharedPreferences("database", Context.MODE_PRIVATE);
-
         activated = preferences.getBoolean("activated", false);
 
         if (activated) {
@@ -85,6 +89,25 @@ public class MainActivity extends AppCompatActivity {
                 editor.putInt("minute", minute);
                 editor.apply();
 
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(Calendar.HOUR_OF_DAY, hour);
+                calendar.set(Calendar.MINUTE, minute);
+
+                Intent notificationIntent = new Intent(MainActivity.this, NotificationPublisher.class);
+                notificationIntent.putExtra(NotificationPublisher.KEY_NOTIFICATION_ID, 1);
+                notificationIntent.putExtra(NotificationPublisher.KEY_NOTIFICATION, "Hora de beber água");
+
+                PendingIntent broadcast = PendingIntent.getBroadcast(MainActivity.this,
+                        0, notificationIntent,
+                        PendingIntent.FLAG_CANCEL_CURRENT);
+
+                AlarmManager alarmManager =
+                        (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+                alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,
+                        calendar.getTimeInMillis(),
+                        interval * 1000 * 60,
+                        broadcast);
             } else {
                 btnNotify.setText(R.string.notify);
                 ColorStateList color = ContextCompat.getColorStateList(v.getContext(), R.color.teal_200);
@@ -96,6 +119,13 @@ public class MainActivity extends AppCompatActivity {
                 editor.remove("hour");
                 editor.remove("minute");
                 editor.apply();
+
+                Intent notificationIntent = new Intent(MainActivity.this, NotificationPublisher.class);
+                PendingIntent broadcast = PendingIntent.getBroadcast(MainActivity.this,
+                        0, notificationIntent, 0);
+                AlarmManager alarmManager =
+                        (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                alarmManager.cancel(broadcast);
             }
 
             activated = !activated;
@@ -103,16 +133,4 @@ public class MainActivity extends AppCompatActivity {
             Log.d("DebugTest", "notifyClick: " + hour + " : " + minute + " : " + interval + ".");
         }
     };
-
-
-//    public void notifyClick(View view){
-//        String sInterval = editMinutes.getText().toString();
-//
-//        hour = timePicker.getCurrentHour();
-//        minute = timePicker.getCurrentMinute();
-//        interval = Integer.parseInt(sInterval);
-//
-//        Log.d("DebugTest", "notifyClick: " + hour + " : " + minute + " : " + interval + ".");
-//    }
-
 }
